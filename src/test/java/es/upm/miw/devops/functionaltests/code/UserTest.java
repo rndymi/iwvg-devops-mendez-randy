d07b5e0e9b6213606e76c64250e54b3ffbdd18fa
Randy <29311604+rndymi@users.noreply.github.com>
2025-09-25 21:26:23 +0200

    fixed User #6

--- src/test/java/es/upm/miw/devops/functionaltests/code/UserTest.java
+++ src/test/java/es/upm/miw/devops/functionaltests/code/UserTest.java
@@ -1,7 +1,22 @@
 package es.upm.miw.devops.functionaltests.code;
 
+import es.upm.miw.devops.code.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserTest {
 
+    @Test
+    void testFullName() {
+        User user = new User("1", "Carlos", "Pérez", null);
+        assertEquals("Carlos Pérez", user.fullName(), "El nombre completo no es correcto");
+    }
 
+    @Test
+    void testInitials() {
+        User user = new User("2", "María", "López", null);
+        assertEquals("M.", user.initials(), "Las iniciales no son correctas");
+    }
 
 }
